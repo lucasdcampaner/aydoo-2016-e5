4cde39286229ccb7602af6f626de1895cc052550
palaciosgabriel@hotmail.com <palaciosgabriel@hotmail.com>
2016-05-22 23:53:13 -0300

    agregado de SectionTest

--- src/test/java/ar/edu/untref/aydoo/SectionTest.java
+++ src/test/java/ar/edu/untref/aydoo/SectionTest.java
@@ -6,28 +6,28 @@ import org.junit.Test;
 public class SectionTest {
 
 	
-//	@Test
-//	public void seObtieneTextoInicioEnFormatoHTMLParaUnaSection() {
-//		
-//		ItemEntrada section = new Section("Comienza la seccion");
-//		String sectionInicioEsperada = "<section>";
-//		
-//		Formateador formateadorHTML = new FormateadorHTML();
-//		String sectionInicioObtenidaFormatoHTML = section.getInicioFormateado(formateadorHTML);
-//		
-//		Assert.assertEquals(sectionInicioEsperada, sectionInicioObtenidaFormatoHTML);
-//	}
-//
-//	@Test
-//	public void seObtieneTextoFinEnFormatoHTMLParaUnaSection() {
-//		
-//		ItemEntrada section = new Section("Finaliza la seccion");
-//		String sectionFinEsperada = "</section>";
-//		
-//		Formateador formateadorHTML = new FormateadorHTML();
-//		String sectionFinObtenidaFormatoHTML = section.getFinFormateado(formateadorHTML);
-//		
-//		Assert.assertEquals(sectionFinEsperada, sectionFinObtenidaFormatoHTML);
-//	}	
+	@Test
+	public void seObtieneTextoInicioEnFormatoHTMLParaUnaSection() {
+		
+		ItemEntrada section = new Section("Comienza la seccion");
+		String sectionInicioEsperada = "<section>";
+		
+		Formateador formateadorHTML = new FormateadorHTML();
+		String sectionInicioObtenidaFormatoHTML = section.getInicioFormateado(formateadorHTML);
+		
+		Assert.assertEquals(sectionInicioEsperada, sectionInicioObtenidaFormatoHTML);
+	}
+
+	@Test
+	public void seObtieneTextoFinEnFormatoHTMLParaUnaSection() {
+		
+		ItemEntrada section = new Section("Finaliza la seccion");
+		String sectionFinEsperada = "</section>";
+		
+		Formateador formateadorHTML = new FormateadorHTML();
+		String sectionFinObtenidaFormatoHTML = section.getFinFormateado(formateadorHTML);
+		
+		Assert.assertEquals(sectionFinEsperada, sectionFinObtenidaFormatoHTML);
+	}	
 	
 }
