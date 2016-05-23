fe012a07d463d98e0a17ab5850ef650030b9dadf
Matías Mansilla <tute.mdm@gmail.com>
2016-05-23 00:36:51 -0300

    Se agrega nuevo test a VisualizadorPorPantallaTest

--- src/test/java/ar/edu/untref/aydoo/VisualizadorPorPantallaTest.java
+++ src/test/java/ar/edu/untref/aydoo/VisualizadorPorPantallaTest.java
@@ -1,5 +1,8 @@
 package ar.edu.untref.aydoo;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -14,10 +17,10 @@ public class VisualizadorPorPantallaTest {
 
 		Formateador formateadorHTML = new FormateadorHTML();
 		String tituloObtenidoFormatoHTML = titulo.getTextoFormateado(formateadorHTML);
-		
+
 		Assert.assertEquals(tituloEsperado, tituloObtenidoFormatoHTML);
 	}
-	
+
 	@Test
 	public void seMuestraContenidoEnFormatoHTMLParaUnSubtituloDePrueba() {
 
@@ -27,8 +30,28 @@ public class VisualizadorPorPantallaTest {
 
 		Formateador formateadorHTML = new FormateadorHTML();
 		String subtituloObtenidoFormatoHTML = subtitulo.getTextoFormateado(formateadorHTML);
-		
+
 		Assert.assertEquals(subtituloEsperado, subtituloObtenidoFormatoHTML);
 	}
 
+	@Test
+	public void seMuestraContenidoEnFormatoHTMLParaUnTituloYUnSubtituloDePrueba() {
+
+		String tituloEntrada = "Titulo de prueba";
+		ItemEntrada titulo = new Titulo(tituloEntrada);
+		String tituloEsperado = "<h1>Titulo de prueba</h1>";
+		String subtituloEntrada = "Subtitulo de prueba";
+		ItemEntrada subtitulo = new SubTitulo(subtituloEntrada);
+		String subtituloEsperado = "<h2>Subtitulo de prueba</h2>";
+		String itemsEsperados = tituloEsperado + subtituloEsperado;
+
+		Formateador formateadorHTML = new FormateadorHTML();
+		List<String> items = new LinkedList<String>();
+		String tituloObtenidoFormatoHTML = titulo.getTextoFormateado(formateadorHTML);
+		String subtituloObtenidoFormatoHTML = subtitulo.getTextoFormateado(formateadorHTML);
+		items.add(tituloObtenidoFormatoHTML);
+		items.add(subtituloObtenidoFormatoHTML);
+
+		Assert.assertEquals(itemsEsperados, items.get(0) + items.get(1));
+	}
 }
