3f97c60ec8f55e28ab8869de93c3e0389c1dff87
lucas <lucasdcampaner@gmail.com>
2016-05-24 15:16:28 -0300

    Primera version de GeneradorItemsDesdeArchivo.

---
+++ src/main/java/ar/edu/untref/aydoo/GeneradorItemsDesdeArchivo.java
@@ -0,0 +1,67 @@
+package ar.edu.untref.aydoo;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class GeneradorItemsDesdeArchivo {
+
+	private FileReader archivoEntrada;
+	private Scanner scanner;
+	
+	public GeneradorItemsDesdeArchivo(String archivoEntrada) throws FileNotFoundException {
+		
+		this.archivoEntrada = new FileReader(archivoEntrada); 
+	}
+	
+	public List<ItemEntrada> getItemsEntrada(Formateador formateador) throws IOException {
+		
+		List<ItemEntrada> itemsEntrada = new LinkedList<ItemEntrada>();
+		scanner = new Scanner(archivoEntrada);
+		ItemEntrada itemEntrada;
+		
+		while (scanner.hasNextLine()) {
+			itemEntrada = getTipoDeItem(scanner.nextLine());
+        	if (itemEntrada != null){
+    			itemsEntrada.add(itemEntrada);
+    		}
+		}
+		
+		return itemsEntrada;
+	}
+	
+	private ItemEntrada getTipoDeItem(String linea) {
+		
+		ItemEntrada itemEntrada = null;
+		String caracterizadorLinea = linea.substring(0, 2);
+		String textoDesdePosicion2 = linea.substring(2);
+		String textoDesdePosicion3 = linea.substring(3);
+		
+		switch (caracterizadorLinea) {
+		case "# ":
+			itemEntrada = new Titulo(textoDesdePosicion2);
+			break;
+		case "##":
+			itemEntrada = new SubTitulo(textoDesdePosicion3);
+			break;
+		case "i:":
+			itemEntrada = new Imagen(textoDesdePosicion3);
+			break;		
+		case "* ":
+			itemEntrada = new ItemLista(textoDesdePosicion2);
+			break;
+		case "--":
+			itemEntrada = new Section("");
+			break;
+		default:
+			itemEntrada = new TextoPlano(linea);
+			break;
+		}
+		
+		return itemEntrada;
+	}
+	
+}
