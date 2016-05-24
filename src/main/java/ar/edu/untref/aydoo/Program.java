e4e4795915c35aeefea52a3387dda379c03c0887
Lucas <lucasdcampaner@gmail.com>
2016-05-24 10:31:32 -0300

    Modificacion de pom.xml para que tome nombre de aplicacion slider y prueba en la clase Program.

--- src/main/java/ar/edu/untref/aydoo/Program.java
+++ src/main/java/ar/edu/untref/aydoo/Program.java
@@ -1,7 +1,34 @@
 package ar.edu.untref.aydoo;
 
 public class Program {
-	public static final void main(String arg[]) {
+	
+	private static String mode;
+	private static String archivoEntrada;
+	private static String output;
+	
+	public static final void main(String args[]) {
 
+		getParametrosAplicacion(args);
+			
+		System.out.println(mode);
+		System.out.println(archivoEntrada);
+		System.out.println(output);
+		//TODO DO
+		/**
+		 * Si mode=default genero la carpeta con el mismo nombre de archivo de entrada.
+		 * Si mode=no-output imprimi por pantalla.
+		 * Si output=carpeta genero la carpeta con el nombre especificado.
+		 */
+		
 	}
+
+	private static void getParametrosAplicacion(String[] args) {
+		
+		ManejadorDeOpciones manejadorDeOpciones = new ManejadorDeOpciones(args);	
+		mode = manejadorDeOpciones.getMode();
+		archivoEntrada = manejadorDeOpciones.getArchivoEntrada();
+		output = manejadorDeOpciones.getOutput();
+	}
+
+
 }
