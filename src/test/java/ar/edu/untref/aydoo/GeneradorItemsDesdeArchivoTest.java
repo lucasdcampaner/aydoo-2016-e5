9e122beaeba70cf9d84b089c0c8f81a57d8072d4
lucas <lucasdcampaner@gmail.com>
2016-05-24 15:16:04 -0300

    Test OK, pero comentado para hacerlo mockeado.

---
+++ src/test/java/ar/edu/untref/aydoo/GeneradorItemsDesdeArchivoTest.java
@@ -0,0 +1,39 @@
+package ar.edu.untref.aydoo;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GeneradorItemsDesdeArchivoTest {
+	
+	//TODO ver si se puede mockear para no ir al file system.
+
+	/*
+	@Test
+	public void seCreaListaDeItemsEntradaDelEjemploIntegrador() throws IOException {
+		
+		String archivoEntrada = "ejemploIntegrador.md";
+		GeneradorItemsDesdeArchivo generadorItemsDesdeArchivo = new GeneradorItemsDesdeArchivo(archivoEntrada);
+		List<ItemEntrada> itemsEntradaEsperada = new LinkedList<ItemEntrada>();
+		itemsEntradaEsperada.add(new Section(""));
+		itemsEntradaEsperada.add(new Titulo("El titulo"));
+		itemsEntradaEsperada.add(new SubTitulo("El Subtitulo"));
+		itemsEntradaEsperada.add(new Section(""));
+		itemsEntradaEsperada.add(new Titulo("Solo un titulo"));
+		itemsEntradaEsperada.add(new Section(""));
+		itemsEntradaEsperada.add(new TextoPlano("solo texto sin nada mas"));
+		Formateador formateadorHTML = new FormateadorHTML();
+		
+		//Aca estaria el mock
+		List<ItemEntrada> itemsEntradaObtenida = generadorItemsDesdeArchivo.getItemsEntrada(formateadorHTML);
+		
+		for (int i = 0; i < itemsEntradaObtenida.size(); i++) {
+			Assert.assertEquals(itemsEntradaEsperada.get(i).getTexto(), itemsEntradaObtenida.get(i).getTexto());
+		}
+		
+	}
+	*/
+}
