3db18dfb3b89770152abeb27e2a23c79d29bb877
Matías Mansilla <tute.mdm@gmail.com>
2016-05-23 22:56:12 -0300

    Nuevo test en la clase VisualizadorPorPantallaTest

--- src/test/java/ar/edu/untref/aydoo/VisualizadorPorPantallaTest.java
+++ src/test/java/ar/edu/untref/aydoo/VisualizadorPorPantallaTest.java
@@ -39,11 +39,11 @@ public class VisualizadorPorPantallaTest {
 
 		String tituloEntrada = "Titulo de prueba";
 		ItemEntrada titulo = new Titulo(tituloEntrada);
-		String tituloEsperado = "<h1>Titulo de prueba</h1>";
+		String tituloEsperado = "<h1>Titulo de prueba</h1>" + "\n";
 		String subtituloEntrada = "Subtitulo de prueba";
 		ItemEntrada subtitulo = new SubTitulo(subtituloEntrada);
-		String subtituloEsperado = "<h2>Subtitulo de prueba</h2>";
-		
+		String subtituloEsperado = "<h2>Subtitulo de prueba</h2>" + "\n";
+
 		List<String> itemsEsperados = new LinkedList<String>();
 		itemsEsperados.add(tituloEsperado);
 		itemsEsperados.add(subtituloEsperado);
@@ -52,9 +52,40 @@ public class VisualizadorPorPantallaTest {
 		VisualizadorPorPantalla items = new VisualizadorPorPantalla();
 		String tituloObtenidoFormatoHTML = titulo.getTextoFormateado(formateadorHTML);
 		String subtituloObtenidoFormatoHTML = subtitulo.getTextoFormateado(formateadorHTML);
-		
+
 		items.agregarItem(tituloObtenidoFormatoHTML);
 		items.agregarItem(subtituloObtenidoFormatoHTML);
+
+		Assert.assertEquals(itemsEsperados, items.mostrarItems());
+	}
+
+	@Test
+	public void seMuestraContenidoEnFormatoHTMLParaUnTituloUnSubtituloYUnaImagenDePrueba() {
+
+		String tituloEntrada = "Titulo de prueba";
+		ItemEntrada titulo = new Titulo(tituloEntrada);
+		String tituloEsperado = "<h1>Titulo de prueba</h1>" + "\n";
+		String subtituloEntrada = "Subtitulo de prueba";
+		ItemEntrada subtitulo = new SubTitulo(subtituloEntrada);
+		String subtituloEsperado = "<h2>Subtitulo de prueba</h2>" + "\n";
+		String imagenEntrada = "imagenDePrueba.jpg";
+		ItemEntrada imagen = new Imagen(imagenEntrada);
+		String imagenEsperada = "<img src=\"imagenDePrueba.jpg\"/>" + "\n";
+
+		List<String> itemsEsperados = new LinkedList<String>();
+		itemsEsperados.add(tituloEsperado);
+		itemsEsperados.add(subtituloEsperado);
+		itemsEsperados.add(imagenEsperada);
+
+		Formateador formateadorHTML = new FormateadorHTML();
+		VisualizadorPorPantalla items = new VisualizadorPorPantalla();
+		String tituloObtenidoFormatoHTML = titulo.getTextoFormateado(formateadorHTML);
+		String subtituloObtenidoFormatoHTML = subtitulo.getTextoFormateado(formateadorHTML);
+		String imagenObtenidaFormatoHTML = imagen.getTextoFormateado(formateadorHTML);
+
+		items.agregarItem(tituloObtenidoFormatoHTML);
+		items.agregarItem(subtituloObtenidoFormatoHTML);
+		items.agregarItem(imagenObtenidaFormatoHTML);
 
 		Assert.assertEquals(itemsEsperados, items.mostrarItems());
 	}
