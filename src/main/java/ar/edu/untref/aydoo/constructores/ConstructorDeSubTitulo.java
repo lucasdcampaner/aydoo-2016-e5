8d448b8a6910a2daacf40f69be96692d93ea76df
Lucas <lucasdcampaner@gmail.com>
2016-06-18 09:26:58 -0300

    Creacion de ConstructorDeSubtitulo.

---
+++ src/main/java/ar/edu/untref/aydoo/constructores/ConstructorDeSubTitulo.java
@@ -0,0 +1,25 @@
+package ar.edu.untref.aydoo.constructores;
+
+import ar.edu.untref.aydoo.dominio.Item;
+import ar.edu.untref.aydoo.dominio.SubTitulo;
+
+public class ConstructorDeSubTitulo extends ConstructorDeItem {
+
+	@Override
+	public Item construir(String texto) {
+
+		Item resultado = null;
+
+		if (texto.startsWith("##")) {
+
+			resultado = new SubTitulo(texto.substring(3, texto.length()));
+			this.detenerConstruccion();
+		} else {
+
+			resultado = this.continuarConstruccion(texto);
+		}
+
+		return resultado;
+	}
+	
+}
---
+++ src/test/java/ar/edu/untref/aydoo/constructores/ConstructorDeSubTituloTest.java
@@ -0,0 +1,21 @@
+package ar.edu.untref.aydoo.constructores;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import ar.edu.untref.aydoo.dominio.Item;
+import ar.edu.untref.aydoo.dominio.SubTitulo;
+
+public class ConstructorDeSubTituloTest {
+
+	@Test
+	public void construirSubTituloMD() {
+		
+		ConstructorDeSubTitulo constructorDeSubTitulo = new ConstructorDeSubTitulo();
+		String SubTitulo = "## SubTitulo";
+		Item itemEsperado = constructorDeSubTitulo.construir(SubTitulo);
+				
+		Assert.assertEquals(SubTitulo.class, itemEsperado.getClass());
+	}
+
+}
