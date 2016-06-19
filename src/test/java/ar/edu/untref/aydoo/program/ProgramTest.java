f54c5ee24e32f1c36755c42f6315deb20d41e728
Lucas <lucasdcampaner@gmail.com>
2016-06-19 12:25:20 -0300

    Indentacion en la clase Program.test.

--- src/test/java/ar/edu/untref/aydoo/program/ProgramTest.java
+++ src/test/java/ar/edu/untref/aydoo/program/ProgramTest.java
@@ -10,14 +10,14 @@ public class ProgramTest {
 	@SuppressWarnings("static-access")
 	@Test
 	public void obtenerParametrosBasicos() throws Exception {
-		
+
 		String mode = "--mode=default";
 		String modeEsperado = "DEFAULT";
 		String archivoEntrada = "mipresentacion1.md";
 		String archivoEntradaEsperado = "mipresentacion1.md";
-		String args[] = {mode, archivoEntrada};
+		String args[] = { mode, archivoEntrada };
 		Program program = new Program();
-		
+
 		program.getParametrosAplicacion(args);
 		String modeObtenido = program.getMode();
 		String archivoEntradaObtenido = program.getArchivoEntrada();
@@ -30,16 +30,16 @@ public class ProgramTest {
 	@SuppressWarnings("static-access")
 	@Test
 	public void obtenerParametrosConCarpetaDeSalida() throws Exception {
-		
+
 		String mode = "--mode=default";
 		String modeEsperado = "DEFAULT";
 		String archivoEntrada = "mipresentacion1.md";
 		String archivoEntradaEsperado = "mipresentacion1.md";
 		String carpetaSalida = "--output=carpetaParaSalida";
 		String carpetaSalidaEsperada = "carpetaParaSalida";
-		String args[] = {mode, archivoEntrada, carpetaSalida};
+		String args[] = { mode, archivoEntrada, carpetaSalida };
 		Program program = new Program();
-		
+
 		program.getParametrosAplicacion(args);
 		String modeObtenido = program.getMode();
 		String archivoEntradaObtenido = program.getArchivoEntrada();
@@ -50,8 +50,5 @@ public class ProgramTest {
 		Assert.assertEquals(archivoEntradaEsperado, archivoEntradaObtenido);
 		Assert.assertEquals(carpetaSalidaEsperada, archivoCarpertaSalidaObtenida);
 	}
-	
-	
-	
-}
 
+}
